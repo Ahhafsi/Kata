4fd55c4e4f2308da0b84755649e960e8cc75d91c
ahafsi <hafsiahamd@gmail.com>
2022-12-04 16:09:47 +0100

    fixe pipe test

--- src/app/core/pipes/taxe.pipe.spec.ts
+++ src/app/core/pipes/taxe.pipe.spec.ts
@@ -1,8 +1,19 @@
 import { TaxePipe } from './taxe.pipe';
+import { TestBed } from '@angular/core/testing';
+import { TaxeService } from '@app/features/cart/taxe.service';
 
 describe('TaxePipe', () => {
+  let service: TaxeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TaxeService],
+    });
+    service = TestBed.inject(TaxeService);
+  });
+
   it('create an instance', () => {
-    const pipe = new TaxePipe();
+    const pipe = new TaxePipe(service);
     expect(pipe).toBeTruthy();
   });
 });
